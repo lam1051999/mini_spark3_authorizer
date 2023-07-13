04f87a29296eb83363fd15eb00f943f2d23d7fe6
lam1051999 <lam1051999.com>
2023-07-13 14:29:29 +0700

    add calculator

--- spark3-ranger-custom/src/main/scala/org/apache/ranger/authorization/spark/authorizer/RangerSparkAuditHandler.scala
+++ spark3-ranger-custom/src/main/scala/org/apache/ranger/authorization/spark/authorizer/RangerSparkAuditHandler.scala
@@ -3,7 +3,4 @@ package org.apache.ranger.authorization.spark.authorizer
 import org.apache.ranger.plugin.audit.RangerDefaultAuditHandler
 
 class RangerSparkAuditHandler extends RangerDefaultAuditHandler {
-
-  // TODO: Implementing meaningfully audit functions
-
 }
